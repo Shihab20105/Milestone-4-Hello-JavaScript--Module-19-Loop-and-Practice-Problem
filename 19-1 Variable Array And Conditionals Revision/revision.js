f11cce2313c247981c7260ce66a3f1b0c6d3dde6
shihab20105 <shihabur10@gmail.com>
2024-03-30 18:15:28 +0000

    Explored Variable Array And Conditionals Revision

---
+++ 19-1 Variable Array And Conditionals Revision/revision.js
@@ -0,0 +1,32 @@
+// String Type Variable
+var bottle = 'water';
+var bottleColor = 'yellow';
+var penColor = 'blue';
+
+// Number Type Variable
+var sunglassPrice = 120;
+
+// Boolean Type Variable
+var isExpensive = true;
+
+var tableItems = ['bottle', 'pen', 'mouse', 'sunglass', 'paper', ];
+
+var item3 = tableItems[3];
+
+tableItems[4] = 'notebook';
+
+var penIndex = tableItems.indexOf(pen);
+
+
+// Push Pop
+
+// Conditionals
+if(tableItems.length > 4){
+
+}
+else if(tableItems[3] == 'pen'){
+    
+}
+else{
+
+}
