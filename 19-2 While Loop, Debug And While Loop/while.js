2be34ca4b21b3360c03af751ca7433ea85d85b7b
shihab20105 <shihabur10@gmail.com>
2024-04-01 00:02:08 +0100

    Explored While Loop, Debug And Understand While Loop

---
+++ 19-2 While Loop, Debug And While Loop/while.js
@@ -0,0 +1,24 @@
+// roastGiven = roastGiven = 1;
+// roastGiven += 1;
+
+roastGiven = 0;
+while(roastGiven < 7){
+    console.log('Give The Roast, Please !!' )
+    console.log(roastGiven);
+    roastGiven++;
+}
+
+// Output:Give The Roast, Please !!
+// 0
+// Give The Roast, Please !!
+// 1
+// Give The Roast, Please !!
+// 2
+// Give The Roast, Please !!
+// 3
+// Give The Roast, Please !!
+// 4
+// Give The Roast, Please !!
+// 5
+// Give The Roast, Please !!
+// 6
