5146aec5098479a5d390d025451849979446b303
shihab20105 <shihabur10@gmail.com>
2024-04-16 19:38:41 +0100

    Exploring For Loop, How For Loop Works, While Vs For Loop

---
+++ 19-4 For Loop, How For Loop Works/for.js
@@ -0,0 +1,132 @@
+/* var roastGiven = 0;
+while(roastGiven < 7){
+    console.log('Give the Roast please !!');
+    roastGiven++;
+} */
+/* Output:
+Give the Roast please !!
+Give the Roast please !!
+Give the Roast please !!
+Give the Roast please !!
+Give the Roast please !!
+Give the Roast please !!
+Give the Roast please !! */
+
+/* For Loop */
+/* for(var roastGiven = 0; roastGiven < 7; roastGiven++){
+    console.log('Give the Roast please !!');
+} */
+/* Output:
+Give the Roast please !!
+Give the Roast please !!
+Give the Roast please !!
+Give the Roast please !!
+Give the Roast please !!
+Give the Roast please !!
+Give the Roast please !! */
+
+/* Simple Version of For Loop */
+/* for(var i = 0; i < 7; i++){
+    console.log(i);
+} */
+/* Output:
+0
+1
+2
+3
+4
+5
+6 */
+
+/* console.log('Simple For Loop');
+for(var i = 0; i < 7; i++){
+    console.log(i);
+} */
+/* Output: Debug console
+Simple For Loop
+0
+1
+2
+3
+4
+5
+6 */
+
+/* for(var i = 0; i < 10; i++){
+    console.log(i);
+} */
+/* Output:
+0
+1
+2
+3
+4
+5
+6
+7
+8
+9 */
+
+/* for(var i = 1; i < 10; i++){
+    console.log(i);
+} */
+/* Output:
+1
+2
+3
+4
+5
+6
+7
+8
+9 */
+
+/* for (var i = 1; i <= 10; i++) {
+    console.log(i);
+} */
+/* Output:
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10 */
+
+
+// i = i + 2;
+// Shorthand i += 2;
+
+/* for (var i = 0; i <= 20; i+=2) {
+    console.log(i);
+} */
+/* Output:
+0
+2
+4
+6
+8
+10
+12
+14
+16
+18
+20 */
+
+for (var i = 1; i <= 20; i+=2) {
+    console.log(i);
+}
+/* Output:
+1
+3
+5
+7
+9
+11
+13
+15
+17
+19 */
