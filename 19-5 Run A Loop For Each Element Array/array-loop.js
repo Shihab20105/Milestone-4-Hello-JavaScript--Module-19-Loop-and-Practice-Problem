fad456577b0a030a8693741759b445ec5f6c1513
shihab20105 <shihabur10@gmail.com>
2024-04-17 01:37:36 +0100

    Explored Run A Loop For Each Element Of An Array

---
+++ 19-5 Run A Loop For Each Element Array/array-loop.js
@@ -0,0 +1,104 @@
+/* for(var i = 0; i < 10; i++){
+    console.log(i);
+} */
+/* Output:
+0
+1
+2
+3
+4
+5
+6
+7
+8
+9 */
+
+// Target: Display every elements of an array 
+/* var numbers = [45, 87, 89, 56, 32, 51, 25];
+
+for(i = 0; i < 7; i++){
+    console.log(i);
+} */
+/* Output:
+0
+1
+2
+3
+4
+5
+6 */
+
+/* var numbers = [45, 87, 89, 56, 32, 51, 25];
+
+for(i = 0; i < 7; i++){
+    var number = numbers[i];
+    console.log(number);
+} */
+/* Output:
+45
+87
+89
+56
+32
+51
+25 */
+
+/* var numbers = [45, 87, 89, 56, 32, 51, 25, 188, 41];
+
+for(i = 0; i < numbers.length; i++){
+    var number = numbers[i];
+    console.log(number);
+} */
+/* Output:
+45
+87
+89
+56
+32
+51
+25
+188
+41 */
+
+
+/* var numbers = [45, 87, 89, 56, 32, 51, 25, 188, 41, 25, 98];
+
+for(i = 0; i < numbers.length; i++){
+    var number = numbers[i];
+    console.log(number);
+} */
+/* Output:
+45
+87
+89
+56
+32
+51
+25
+188
+41
+25
+98 */
+
+/* var items = ['Bottle', 'Mouse', 'Sunglass', 'Pen'];
+for(var i = 0; i < items.length; i++){
+    var item = items[i];
+    console.log(item);
+} */
+/* Output:
+Bottle
+Mouse
+Sunglass
+Pen */
+
+var items = ['Bottle', 'Mouse', 'Sunglass', 'Pen', 'Notebook'];
+for(var i = 0; i < items.length; i++){
+    var item = items[i];
+    console.log(item);
+}
+/* Output:
+Bottle
+Mouse
+Sunglass
+Pen
+Notebook */
