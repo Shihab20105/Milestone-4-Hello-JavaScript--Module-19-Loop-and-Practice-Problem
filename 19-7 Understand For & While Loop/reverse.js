95a3b4b8285a3ca31c1480ad62cfa75f913ff930
shihab20105 <shihabur10@gmail.com>
2024-04-22 21:11:03 +0100

    Explored 19-7 (Advanced) Understand For And While Loop In A Reverse Way

---
+++ 19-7 Understand For & While Loop/reverse.js
@@ -0,0 +1,113 @@
+var num = 0;
+while(num < 10){
+    console.log(num);
+    num++;
+}
+/* Output:
+0
+1
+2
+3
+4
+5
+6
+7
+8
+9 */
+
+var num = 1;
+while(num <= 10){
+    console.log(num);
+    num++;
+}
+/* Output:
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10 */
+
+var num = 10;
+while(num > 1){
+    console.log(num);
+    num--;
+}
+/* Output:
+10
+9
+8
+7
+6
+5
+4
+3
+2 */
+
+
+// While Loop In A Reverse Way
+var num = 10;
+while(num >= 1){
+    console.log(num);
+    num--;
+}
+/* Output:
+10
+9
+8
+7
+6
+5
+4
+3
+2
+1 */
+
+for(var i = 1; i <= 10; i++){
+    console.log(i);
+}
+/* Output:
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10 */
+
+for(var i = 10; i > 1; i--){
+    console.log(i);
+}
+/* Output:
+10
+9
+8
+7
+6
+5
+4
+3
+2 */
+
+
+for(var i = 10; i >= 1; i--){
+    console.log(i);
+}
+/* Output:
+10
+9
+8
+7
+6
+5
+4
+3
+2
+1 */
